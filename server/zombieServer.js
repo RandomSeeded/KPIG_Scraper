ffdcae570e6a2593ab6a333e9016c99f55e362ef
Nate Willard <nate.a.willard@gmail.com>
2015-09-25 20:55:25 -0700

    Add proof of concept scraper using cheerio and zombie

--- server/zombieServer.js
+++ server/zombieServer.js
@@ -1,10 +1,18 @@
 var user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20';
 var Browser = require('zombie');
-var browser = new Browser({userAgent: user_agent, debug: true, waitFor: 10000});
+var browser = new Browser({userAgent: user_agent, debug: true, waitFor: 15000});
+var cheerio = require('cheerio');
 
 var url = 'http://www.kpig.com/playlist.htm';
 
-browser.visit(url, function() {
-  console.log(browser.html());
+browser.visit(url, function(e) {
+  if (e) console.log(e);
+  else {
+    var data = browser.html();
+    var $ = cheerio.load(data);
+    var songinfo = $('#playlist_1 .songinfo');
+    var artist = songinfo.children().first().text();
+    console.log(artist);
+  }
 });
 
